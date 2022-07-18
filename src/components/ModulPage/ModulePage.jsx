9bd9f2eb7c1fb2aa08d3383a6f41b55777bbcb47
= <=>
2022-07-18 20:10:58 +0500

    1

--- src/components/ModulPage/ModulePage.jsx
+++ src/components/ModulPage/ModulePage.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./ModulePage.css";
+import {
+  AiOutlineCalendar,
+  AiOutlineHome,
+  AiOutlineVideoCameraAdd,
+} from "react-icons/ai";
+import { IoIosLogIn } from "react-icons/io";
+import { BsCalendar2Week, BsCardList, BsChatDots } from "react-icons/bs";
+import { BiBookBookmark } from "react-icons/bi";
+import Accordion from "../Module/Accordion";
+
 function ModulePage() {
   function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -15,16 +25,81 @@ function ModulePage() {
         <Link to="#" className="closebtn" onClick={closeNav}>
           &times;
         </Link>
-        <Link to="#">About</Link>
-        <Link to="#">Services</Link>
-        <Link to="#">Clients</Link>
-        <Link to="#">Contact</Link>
+        <div
+          style={{ flexDirection: "column" }}
+          className="container-fluid d-flex justify-content-evenly align-items-center"
+        >
+          <div className="avatar"></div>
+          <h6 style={{ color: "#747483" }} className="text-center m-0">
+            Malikov Abdulboriy
+          </h6>
+          <div className="linear-div my-2"></div>
+          <p style={{ color: "#747483" }} className="text-center m-0">
+            Semester One
+          </p>{" "}
+          <p style={{ color: "#747483" }} className="text-center m-0">
+            3BUSS001C
+          </p>{" "}
+          <p style={{ color: "#747483" }} className="text-center mb-5">
+            Quantitative Methods
+          </p>
+        </div>
+        <div className="container-fluid p-0">
+          <Link to="#">
+            <button className="d-flex align-items-center btn w-100 nav-btn-color  p-2">
+              <BsCardList />
+              <h6 className="mt-1 mx-2">Lessons</h6>
+            </button>
+          </Link>
+          <Link to="#">
+            <button className="d-flex btn w-100 align-items-center nav-btn-color  p-2">
+              <AiOutlineVideoCameraAdd />
+              <h6 className="mt-1 mx-2">Webinars</h6>
+            </button>
+          </Link>
+          <Link to="#">
+            <button className="d-flex w-100 btn align-items-center nav-btn-color  p-2">
+              <BsChatDots />
+              <h6 className="mt-1 mx-2">Discussion </h6>
+            </button>
+          </Link>
+          <Link to="#">
+            <button className="d-flex w-100 btn align-items-center nav-btn-color  p-2">
+              <BiBookBookmark />
+              <h6 className="mt-1 mx-2">Coursework</h6>
+            </button>
+          </Link>
+        </div>
       </div>
 
-      <span style={{ fontSize: "30px", cursor: "pointer" }} onClick={openNav}>
-        &#9776; open
-      </span>
+      <div className="d-flex container mt-2 justify-content-between">
+        <div style={{ fontSize: "30px", cursor: "pointer" }} onClick={openNav}>
+          &#9776;
+        </div>
+        <div className="nav-container  w-50 d-flex justify-content-evenly align-items-center">
+          <div className=" nav-icon-border ">
+            <h2>
+              <AiOutlineHome />
+            </h2>
+          </div>
+          <div className="nav-icon-border">
+            <h2>
+              <BsCalendar2Week />
+            </h2>
+          </div>
+          <div className="nav-icon-border">
+            <h2>
+              <IoIosLogIn />
+            </h2>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="container">
+        <Accordion />
+      </div>
     </>
+    // F5FBFF
   );
 }
 
